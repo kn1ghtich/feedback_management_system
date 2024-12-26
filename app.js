5ef3f3ad095432e590d493d1cdcf05cb04db742a
jmiha <jmihairlines27@gmail.com>
2024-12-26 18:07:04 +0500

    Group-D

--- app.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-console.log("BIMBIM");
-
-fs.writeFileSync('test.txt', 'Hello World');
--- ho.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.end('Hello World');
-}).listen(8080);
